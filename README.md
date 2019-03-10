d507c1c3429e91f2a8bb936e963914af112f9916
defunctec <defuncteconomics@gmail.com>
2019-03-10 18:05:26 +0000

    Copy to testnet
    Edit to work with testnet

---
+++ README.md
@@ -0,0 +1,27 @@
+# CrownPi-Update 12.7.3
+> Local Masternode hosting for the Crown(CRW) community.
+
+![](http://i63.tinypic.com/vxke4x.png)
+
+## Guide
+
+1. Once logged into your device use this command to initiate the update process
+```sh
+sudo wget "https://raw.githubusercontent.com/defunctec/CrownPi-Update/master/crownpiscript.sh" -O update.sh | bash && sudo chmod +x update.sh && sudo ./update.sh
+```
+2. Once finished updating restart the Crown client by using
+```sh
+sudo crownd
+```
+
+## Usage
+
+Raspberry Pi - Raspian Stretch Lite:
+
+```sh
+sudo wget "https://raw.githubusercontent.com/defunctec/CrownPi-Update/master/crownpiscript.sh" -O update.sh | bash && sudo chmod +x update.sh && sudo ./update.sh
+```
+
+## About Crown
+
+HomePage - https://Crown.tech
---
+++ crownpiscript.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Clear Screen
+clear_screen() {
+     clear   
+     sleep 3
+     echo CrownPi is now updating...
+     sleep 2
+}
+
+# Update OS
+update_repos() {
+    echo Updateing OS, please wait...
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+}
+
+# Download Crown client (Update link with new client)
+download_package() {
+    # Password change prompt
+    echo Getting 0.12.7.3
+    # Create temporary directory
+    dir=`mktemp -d`
+    if [ -z "$dir" ]; then
+        # Create directory under $HOME if above operation failed
+        dir=$HOME/crown-temp
+        mkdir -p $dir
+    fi
+    # Change this later to take latest release version.
+    sudo wget "https://gitlab.crown.tech/crown/crown-core/-/jobs/4986/artifacts/download" -O $dir/crown.zip
+}
+
+# Install Crown client
+install_package() {
+    echo Installing Crown client...
+    sudo unzip -d $dir/crown $dir/crown.zip
+    sudo cp -f $dir/crown/*/bin/* /usr/local/bin/
+    sudo cp -f $dir/crown/*/lib/* /usr/local/lib/
+    sudo rm -rf $tmp
+}
+
+# CrownPi package
+main() {
+    # Clear screen
+    clear_screen
+    # Stop crownd (in case it's running)
+    sudo crown-cli stop
+    # Update Repos
+    update_repos
+    # Install Packages
+    download_package
+    # Extract and install
+    install_package
+}
+
+main
+
+# Notes
+echo Please use "sudo crownd" to start the client.
